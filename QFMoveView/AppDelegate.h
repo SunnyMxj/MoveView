5fa74f32fb24480a73fc8ed13015914dfefa43c3
Ryan <253781200@qq.com>
2016-08-22 17:16:18 +0800

    initial commit

---
+++ QFMoveView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QFMoveView
+//
+//  Created by QianFan_Ryan on 16/8/22.
+//  Copyright © 2016年 QianFan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
